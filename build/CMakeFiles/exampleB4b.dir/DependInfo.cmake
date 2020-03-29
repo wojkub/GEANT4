168ee39f29dfe09dbed82d56e4a4be2d84210947
Wojciech Kubinski <w.r.kubinski@gmail.com>
2020-03-29 11:46:37 +0200

    B4_example v1

---
+++ build/CMakeFiles/exampleB4b.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wojciech/GEANT4/B4b/exampleB4b.cc" "/home/wojciech/GEANT4/B4b/build/CMakeFiles/exampleB4b.dir/exampleB4b.cc.o"
+  "/home/wojciech/GEANT4/B4b/src/B4DetectorConstruction.cc" "/home/wojciech/GEANT4/B4b/build/CMakeFiles/exampleB4b.dir/src/B4DetectorConstruction.cc.o"
+  "/home/wojciech/GEANT4/B4b/src/B4PrimaryGeneratorAction.cc" "/home/wojciech/GEANT4/B4b/build/CMakeFiles/exampleB4b.dir/src/B4PrimaryGeneratorAction.cc.o"
+  "/home/wojciech/GEANT4/B4b/src/B4bActionInitialization.cc" "/home/wojciech/GEANT4/B4b/build/CMakeFiles/exampleB4b.dir/src/B4bActionInitialization.cc.o"
+  "/home/wojciech/GEANT4/B4b/src/B4bEventAction.cc" "/home/wojciech/GEANT4/B4b/build/CMakeFiles/exampleB4b.dir/src/B4bEventAction.cc.o"
+  "/home/wojciech/GEANT4/B4b/src/B4bRunAction.cc" "/home/wojciech/GEANT4/B4b/build/CMakeFiles/exampleB4b.dir/src/B4bRunAction.cc.o"
+  "/home/wojciech/GEANT4/B4b/src/B4bRunData.cc" "/home/wojciech/GEANT4/B4b/build/CMakeFiles/exampleB4b.dir/src/B4bRunData.cc.o"
+  "/home/wojciech/GEANT4/B4b/src/B4bSteppingAction.cc" "/home/wojciech/GEANT4/B4b/build/CMakeFiles/exampleB4b.dir/src/B4bSteppingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4MULTITHREADED"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/wojciech/GEANT4/geant4-install/include/Geant4"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtCore"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtOpenGL"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
